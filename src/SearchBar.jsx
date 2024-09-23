26c6a7c4b9c9c34a91394f2f5757d2a4707edacf
Fofo <127132293+fk-crafter@users.noreply.github.com>
2024-09-23 22:13:18 +0200

    Add files via upload

---
+++ src/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="Search Pokemon..." />
+    </div>
+  );
+};
+
+export default SearchBar;
